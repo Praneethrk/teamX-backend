9ca23f51ecfb46cd759208867a4569f0912c8254
Praneethrk17 <praneethrk17@gmail.com>
2020-02-05 16:44:22 +0530

    first page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify,request
 from flask_cors import CORS, cross_origin
+import statement1dbope as db
 from flask_pymongo import PyMongo
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity, get_jwt_claims
 
 )
-import statement4db as db
 app = Flask(__name__)
 CORS(app)
 
@@ -17,20 +17,21 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/dhi_analytics"
 mongo = PyMongo(app)
 # Setup the Flask-JWT-Extended extension
 
-
 app.config['JWT_SECRET_KEY'] = 'super-secret' 
 jwt = JWTManager(app)
 
 
 class UserObject:
-    def __init__(self, username, roles):
+    def __init__(self, username, roles,emlpoyeeGivenId,usn):
         self.username = username
         self.roles = roles
-
+        self.emlpoyeeGivenId = emlpoyeeGivenId
+        self.usn = usn
+ 
 
 @jwt.user_claims_loader
 def add_claims_to_access_token(user):
-    return {'roles': user.roles}
+    return {'roles': user.roles,"emlpoyeeGivenId":user.emlpoyeeGivenId,"usn":user.usn}
 
 @jwt.user_identity_loader
 def user_identity_lookup(user):
@@ -41,6 +42,8 @@ def user_identity_lookup(user):
 # it to the caller however you choose.
 @app.route('/login', methods=['POST'])
 def login():
+    emlpoyeeGivenId = ''
+    usn = ''
     if not request.is_json:
         return jsonify({"msg": "Missing JSON in request"}), 400
     username = request.json.get('username', None)
@@ -50,7 +53,11 @@ def login():
     if not user:
         return jsonify({"msg": "Bad username or password"}), 401
     roles = [ x['roleName'] for x in user['roles']]
-    user = UserObject(username=user["email"], roles=roles)
+    if 'employeeGivenId' in user:
+        emlpoyeeGivenId = user["employeeGivenId"]
+    if 'usn' in user:
+        usn = user["usn"]
+    user = UserObject(username=user["email"], roles=roles,emlpoyeeGivenId = emlpoyeeGivenId,usn = usn)
     # Identity can be any data that is json serializable
     access_token = create_access_token(identity=user,expires_delta=False)
     return jsonify(access_token=access_token), 200
@@ -58,6 +65,8 @@ def login():
 @app.route('/message')
 def message():
     return {"message":"Check you luck"}
+
+
 
 # Protect a view with jwt_required, which requires a valid access token
 # in the request to access.
@@ -68,24 +77,29 @@ def message():
 def protected():
     # Access the identity of the current user with get_jwt_identity
     ret = {
-        'user': get_jwt_identity(),  
-        'roles': get_jwt_claims()['roles'] ,
-        }
+            'user': get_jwt_identity(),  
+            'roles': get_jwt_claims()['roles'] ,
+            'employeeGivenId':get_jwt_claims()['emlpoyeeGivenId'],
+            'usn':get_jwt_claims()['usn']
+          }
+        
     return jsonify(ret), 200
 
-@app.route("/get_academic_years")
-def get_academic_years():
-    res = db.get_academic_years()
-    return jsonify({"res":res})
 
-@app.route("/get_semesters")
-def get_semesters_details():
-    res = db.get_semesters()
-    return jsonify({"res":res})
-    
-@app.route("/get_user_usn/<email>")
+@app.route('/academicyear')
+def getacademicYear():
+    year = db.getacademicYear()
+    return jsonify({'acdemicYear':year})
+
+@app.route('/termNumber')
+def get_term_numbers():
+    terms = db.get_term_numbers()
+    return jsonify({'term_numbers':terms})
+
+  
+@app.route("/get_user_dept/<email>")
 def get_user_email(email):
-    res = db.get_user_usn(email)
+    res = db.get_user_dept(email)
     return jsonify({"res":res})
 
 @app.route("/get_placement_offer/<term>/<usn>")
@@ -93,6 +107,10 @@ def get_placement_offer(term,usn):
     res = db.get_placment_offers(term,usn)
     return jsonify({"res":res})
 
+@app.route("/get_uescore/<year>/<dept>/<usn>/<term>")
+def get_uescore(year,dept,usn,term):
+    res = db.get_uescore(year,dept,usn,term)
+    return jsonify({"res":res})
 
 if __name__ == "__main__":
-    app.run(port=8088,debug=True)
+    app.run(debug=True)
---
+++ statement1dbope.py
@@ -0,0 +1,75 @@
+from pymongo import MongoClient
+import pymongo
+from pprint import pprint
+
+db = MongoClient()
+mydb = db.dhi_analytics
+dhi_internal = mydb['dhi_internal']
+dhi_term_details = mydb['dhi_term_detail']
+dhi_student_attendance = mydb['dhi_student_attendance']
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+db = myclient["dhi_analytics"]
+def getacademicYear():
+    academicYear = dhi_internal.aggregate([{"$group":{"_id":"null",
+    "academicYear":{"$addToSet":"$academicYear"}}},{"$project":{"academicYear":"$academicYear","_id":0}}])
+    for year in academicYear:
+        year = year['academicYear']
+    return year
+
+def get_term_numbers():
+    terms_numbers = dhi_term_details.aggregate([ 
+        { "$unwind":"$academicCalendar"}, 
+        {"$group":{"_id":"null","termNumber":{"$addToSet":"$academicCalendar.termNumber"}}},
+        {"$project":{"_id":0}}
+    ])
+    for term in terms_numbers:
+        terms = term['termNumber']
+    terms.sort()
+    return terms
+
+
+def get_user_dept(email):
+    collection = db.dhi_user
+    usn = collection.aggregate([
+        {"$match":{"email":email}},
+        {"$project":{"_id":0,"usn":1}}
+        ])
+    res = []
+    for x in usn:
+        res = x["usn"]
+    res = res[5:7]
+    return res
+
+def get_placment_offers(term, usn):
+
+    collection=db.pms_placement_student_details
+    offers = collection.aggregate([
+        {"$unwind":"$studentList"},
+        {"$match":{"studentList.regNo":usn,"academicYear":term}},
+        {"$project":{"companyName":1,"salary":1,"_id":0}}
+    ])
+    res = []
+    for x in offers:
+        res.append(x)
+    return res
+
+def get_uescore(year,dept,usn,term):
+
+    collection = db.pms_university_exam
+
+    ueScore = collection.aggregate([
+    {"$match":{"academicYear" : year,"deptId" : dept}},
+    {"$unwind":"$terms"},
+    {"$match":{"terms.termNumber":term}},
+    {"$unwind":"$terms.scores"},
+    {"$match":{"terms.scores.usn":usn}},
+    {"$project":{"course":"$terms.scores.courseScores"}},
+    {"$unwind":"$course"},
+    {"$project":{"courseName":"$course.courseName","ueScore":"$course.ueScore","maxUeScore":"$course.maxUeScore","totalScore":"$course.totalScore","_id":0}}
+    ])
+    res = []
+    for x in ueScore:
+        res.append(x)
+    return res
+    
+
